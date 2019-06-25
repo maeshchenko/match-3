491605e040dbc7de793803d6fd283a6197933e7c
mikhail.eshchenko <mikhail.eshchenko@gmail.com>
2019-06-25 13:03:16 +0300

    createField.js

---
+++ createField.js
@@ -0,0 +1,13 @@
+function createField(size){
+  let field = document.createElement('div');
+  field.classList.add('field');
+  field.style.width = size.width + 'em';
+  field.style.height = size.height + 'em';
+
+  let ntx = generateMatrix(size);
+  field.cells = ntx.map(createCell);
+  field.cells.forEach(function(cell){
+    field.append(cell.element);
+  });
+  return field;
+}
